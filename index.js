f44944dd28ca2436cb6783e51f8a8370496b6b31
Jack Skiles <jackskiles420@gmail.com>
2020-05-01 15:07:31 -0400

    changes to allow adding to watchlist

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const search = document.getElementById('search-form');
 const movieList = document.getElementById('movies-container');
 let haveWatched = [];
+let movieArray;
+
 
 function saveToWatchList(imdbID) {
-    let movie = movieData.find(currentMovie => currentMovie.imdbID == imdbID); 
+    let movie = movieArray.find(currentMovie => currentMovie.imdbID == imdbID); 
     let watchlistJSON = localStorage.getItem('watchlist');
 
     let watchlist = JSON.parse(watchlistJSON);
@@ -22,18 +24,30 @@ function saveToWatchList(imdbID) {
 
 search.addEventListener('submit', function(e) {
     e.preventDefault();
-    let movieHtmlArray = movieData.map(function (currentMovie) {
-        return `
-        <div>
-            <img src="${currentMovie.Poster}">
-                <div class="card-body">
-                    <h4 class="card-text mx-auto">${currentMovie.Title}</h4>
-                    <p>${currentMovie.Year}</p>
-                    <button class="add" id="add" onclick="saveToWatchList('${currentMovie.imdbID}')">Add To Favorites</button>
-                </div>
-        </div>`
-    });
-    movieList.innerHTML = movieHtmlArray.join('')
+    const searchString = document.querySelector('input').value;
+    const urlEncodedSearchString = encodeURIComponent(searchString);
+    axios.get( "http://www.omdbapi.com/?apikey=b43843a0&s=" + urlEncodedSearchString)
+    .then(function (response) {
+        let movieData = response.data.Search;
+        console.log('movieData');
+        console.log(movieData);
+        let movieHtmlArray = movieData.map(function (currentMovie) {
+            return `
+            <div>
+                <img src="${currentMovie.Poster}">
+                    <div class="card-body">
+                        <h4 class="card-text mx-auto">${currentMovie.Title}</h4>
+                        <p>${currentMovie.Year}</p>
+                        <button class="add" id="add" onclick="saveToWatchList('${currentMovie.imdbID}')">Add To Favorites</button>
+                    </div>
+            </div>`
+        });
+        movieList.innerHTML = movieHtmlArray.join('')
+        movieArray = movieData;
+        console.log(movieArray);
+        // console.log(movieData);
+    })
+    
 
     // return movieHtmlArray.join(' ');
 });
